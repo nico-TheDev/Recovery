0b2b271c385152e5df59ce5832541836bebe2344
Norberto Ignacio <norbertoignaciojr@gmail.com>
2021-05-31 14:29:47 +0800

    Death and respawn

---
+++ Assets/Scripts/DeathAndRespawn.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class DeathAndRespawn : MonoBehaviour
+{
+    // Start is called before the first frame update
+    string currentScene;
+    void Start()
+    {
+        Scene scene = SceneManager.GetActiveScene();
+        currentScene = scene.name;
+    }
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        print("Collided with enemy or death");
+        print(other.gameObject.tag);
+        if (other.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene(currentScene);
+        }
+    }
+}
