0824ffde557d971270321576a4a1038c0d584d9b
Norberto Ignacio <norbertoignaciojr@gmail.com>
2021-05-31 10:58:04 +0800

    implemented camera follow

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform player;
+    public Vector3 offset;
+    [Range(2, 10)]
+    public float transSpeed;
+    void FixedUpdate()
+    {
+        Follow();
+    }
+
+    void Follow()
+    {
+        Vector3 targetPosition = player.position + offset;
+        Vector3 smoothTransition = Vector3.Lerp(transform.position, targetPosition, transSpeed * Time.fixedDeltaTime);
+        transform.position = targetPosition;
+    }
+}
