3e5a4ffa16c24372760fa73a4cc168ca25b6619b
Norberto Ignacio <norbertoignaciojr@gmail.com>
2021-06-01 17:37:28 +0800

    test Menu and Scene Loader

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class SceneLoader : MonoBehaviour
+{
+    public void ContinueGame()
+    {
+        SceneManager.LoadScene("TestScene");
+    }
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+}
