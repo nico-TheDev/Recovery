dfd9837fcb7b4a2d270417b264d5dccecec93f3b
Norberto Ignacio <norbertoignaciojr@gmail.com>
2021-06-15 09:52:39 +0800

    death count changes

--- Assets/Scripts/DeathAndRespawn.cs
+++ Assets/Scripts/DeathAndRespawn.cs
@@ -5,18 +5,31 @@ public class DeathAndRespawn : MonoBehaviour
 {
 
     UIManager loader;
-
+    int currentLevel;
+    string deathCount = "deathCount";
     void Start()
     {
         loader = FindObjectOfType<UIManager>();
+        currentLevel = PlayerPrefs.GetInt("PlayerCurrentLevel");
     }
     void OnTriggerEnter2D(Collider2D other)
     {
-        print("Collided with enemy or death");
-        print(other.gameObject.tag);
         if (other.gameObject.tag == "Player")
         {
             other.gameObject.SetActive(false);
+
+            if (PlayerPrefs.GetInt(deathCount) == 0)
+            {
+                PlayerPrefs.SetInt(deathCount, 1);
+            }
+            else
+            {
+                PlayerPrefs.SetInt(deathCount, PlayerPrefs.GetInt(deathCount) + 1);
+
+            }
+
+            loader.SetDeathCount(PlayerPrefs.GetInt(deathCount));
+            loader.HidePauseBtn();
             loader.ShowDeathPanel();
 
         }
